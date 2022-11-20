21db19ccea9f43ac432871ba17ee4a908e4b49b1
root <root@localhost.localdomain>
2022-11-20 15:06:22 -0800

    start a UI for U3 	modified:   smtmain.py

---
+++ smtmain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+print("[*] Receiving funds.")
+print("[*] this is Wallet B\n\n")
+
+
+#### Sending funds
+print("Enter the wallet id: ")
+#wid_b = input().strip()
+wid_b = "1234"
+
+
+if __name__ == "__main__":
+
+    print("\n\n\n")
+    print("python main!")
+    print("python main!")
+    print("\n")
+
+    print("[+] Wallet ID: " + wid_b)
+    print("\n\n\n")
+
