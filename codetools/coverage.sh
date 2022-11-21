fae18f6e9b9bf73f83bbd5667642b84ea39c7787
root <root@localhost.localdomain>
2022-11-21 02:22:40 -0800

    updated codecoverage reporting (exclude files and generate html) 	modified:   codetools/.coveragerc 	modified:   codetools/coverage.sh

--- codetools/coverage.sh
+++ codetools/coverage.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
 
 # run from project root directory as ./codetools/coverage.sh
+# or specify --rcfile=path/filename as an argument when running manually
 echo "[*] [$0]: **** Running tests with coverage ****"
 echo ""
 COVERAGE_RCFILE=./codetools/.coveragerc coverage run -m unittest discover ./tests/
 
 echo "[*] [$0]"
+echo "**** Generate HTML Coverage Report **** "
+COVERAGE_RCFILE=./codetools/.coveragerc coverage html
+echo "report index: file://$(pwd)/coverage_html_report/index.html"
+
+echo "[*] [$0]"
 echo "**** Coverage Report **** "
-coverage report -m
+COVERAGE_RCFILE=./codetools/.coveragerc coverage report -m
 
