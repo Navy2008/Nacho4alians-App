cc681ab15e9fa9d2a9648830bdf9c579786a3dae
root <root@localhost.localdomain>
2022-11-21 00:47:49 -0800

    initial testcase for smtmain 	new file:   smtmain_test.py

---
+++ smtmain_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import unittest
+import smtmain
+
+class TestSmtmain(unittest.TestCase):
+    """Unittest for smtmain"""
+    def setUp(self):
+        self.smtmain = smtmain.hellofunction()
+
+    def test_validate_hellofunction(self):
+        self.assertEqual(smtmain.hellofunction(), 32)
+
+
+if __name__ == '__main__':
+    unittest.main()
