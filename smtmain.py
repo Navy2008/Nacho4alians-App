ced202cd90a971fd75fdcb381886e81c1d8f87a6
root <root@localhost.localdomain>
2022-11-21 02:54:58 -0800

    pep8 edits 	modified:   smtmain.py

--- smtmain.py
+++ smtmain.py
@@ -3,17 +3,17 @@
 print("[*] Receiving funds.")
 print("[*] this is Wallet B\n\n")
 
-
 #### U3: Sending funds
 print("Enter ID of the receiving wallet: ")
-#wid_b = input().strip()
+# wid_b = input().strip()
 wid_b = "0010"
 print("Enter Amount of funds to add/remove from the wallet (-XXX for remove): ")
-#wid_b_amount = input().strip()
+# wid_b_amount = input().strip()
 wid_b_amount = "1234"
 
 """:counter b: counter value"""
 cb = 1234
+
 
 def hellofunction(z=0):
     """A stub function for starting the U3: sending funds function (see testcase)
@@ -26,7 +26,6 @@ def hellofunction(z=0):
 
 
 if __name__ == "__main__":
-
     print("\n\n\n")  # pragma: no cover
 
     print("python main!")
@@ -41,9 +40,4 @@ if __name__ == "__main__":
     print("[+] Amount wid_b_amount: " + str((initial_amount + int(wid_b_amount))))
     print("\n\n\n")
 
-
-
-
     print("[+] ****** DONE. ********")  # pragma: no cover
-
-
