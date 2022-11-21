30e20a88557b4f9c4568dcee455e9f274f123060
root <root@localhost.localdomain>
2022-11-21 01:46:15 -0800

    updated coverage report generation output formatting 	modified:   codetools/coverage.sh

--- codetools/coverage.sh
+++ codetools/coverage.sh
@@ -1,7 +1,11 @@
 #!/bin/bash 
 
 # run from project root directory as ./codetools/coverage.sh
+echo "[*] [$0]: **** Running tests with coverage ****"
+echo ""
 COVERAGE_RCFILE=./codetools/.coveragerc coverage run -m unittest discover ./tests/
 
+echo "[*] [$0]"
+echo "**** Coverage Report **** "
 coverage report -m
 
